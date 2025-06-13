3c983bec4bf80956730a0296b17bd0098a89a41a
Lim Young <lim.young@foxmail.com>
2025-06-13 16:06:46 +0800

    Completed the basic framework of the input system

---
+++ Source/GameplayInputSystem/Private/GameplayInputSubsystem.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Lim Young.
+
+
+#include "GameplayInputSubsystem.h"
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayInput, "GameplayInput",
+                               "Tag used to identify gameplay input actions. This tag is used to differentiate between gameplay-related inputs and other types of inputs.")
+
+void UGameplayInputSubsystem::InjectGameplayInput(const FGameplayTag& InputTag, const EGameplayInputType InputType) const
+{
+	if (!InputTag.MatchesTag(GameplayInput))
+	{
+		return;
+	}
+
+	OnGameplayInputEvent.Broadcast(InputTag, InputType);
+}
---
+++ Source/GameplayInputSystem/Public/GameplayInputSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Copyright Lim Young.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "NativeGameplayTags.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameplayInputSubsystem.generated.h"
+
+UENUM(BlueprintType)
+enum class EGameplayInputType : uint8
+{
+	Pressed,
+	Released
+};
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayInput)
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYINPUTSYSTEM_API UGameplayInputSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGameplayInputEvent, FGameplayTag, InputTag, EGameplayInputType,
+	                                             InputType);
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Input System|Events")
+	FOnGameplayInputEvent OnGameplayInputEvent;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Input System", meta = (GameplayTagFilter = GameplayInput))
+	void InjectGameplayInput(const FGameplayTag& InputTag, const EGameplayInputType InputType) const;
+};
