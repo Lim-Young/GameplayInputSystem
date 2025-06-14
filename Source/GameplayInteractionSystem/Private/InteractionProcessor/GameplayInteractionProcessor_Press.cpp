53fe9f60316a6a99fc6d717c44c71d01af28515d
Lim Young <lim.young@foxmail.com>
2025-06-15 04:55:39 +0800

    Add Press Processor

---
+++ Source/GameplayInteractionSystem/Private/InteractionProcessor/GameplayInteractionProcessor_Press.cpp
@@ -0,0 +1,58 @@
+﻿// Copyright Lim Young.
+
+
+#include "InteractionProcessor/GameplayInteractionProcessor_Press.h"
+
+void UGameplayInteractionProcessor_Press::OnPressTimeout() const
+{
+	CompleteInteraction(false);
+}
+
+void UGameplayInteractionProcessor_Press::PostInitialize_Implementation()
+{
+	if (Timeout > 0)
+	{
+		if (OwnerWorld->GetTimerManager().IsTimerActive(PressTimeoutHandle))
+		{
+			OwnerWorld->GetTimerManager().ClearTimer(PressTimeoutHandle);
+		}
+
+		OwnerWorld->GetTimerManager().SetTimer(PressTimeoutHandle, this,
+		                                       &UGameplayInteractionProcessor_Press::OnPressTimeout, Timeout, false);
+	}
+}
+
+void UGameplayInteractionProcessor_Press::OnGameplayInputEvent_Implementation(const FGameplayTag& InputTag,
+                                                                              const EGameplayInputType InputType)
+{
+	if (InputType == EGameplayInputType::Released)
+	{
+		// If the input is released, we do not complete the interaction.
+		return;
+	}
+
+	if (InputTag.MatchesTag(PressInputTag) && InputType == EGameplayInputType::Pressed)
+	{
+		CompleteInteraction(true);
+	}
+	else
+	{
+		if (bAccumulatePress)
+		{
+			CompleteInteraction(false);
+		}
+	}
+}
+
+void UGameplayInteractionProcessor_Press::PreCleanup_Implementation()
+{
+	if (!(Timeout > 0))
+	{
+		return;
+	}
+
+	if (OwnerWorld->GetTimerManager().IsTimerActive(PressTimeoutHandle))
+	{
+		OwnerWorld->GetTimerManager().ClearTimer(PressTimeoutHandle);
+	}
+}
---
+++ Source/GameplayInteractionSystem/Public/InteractionProcessor/GameplayInteractionProcessor_Press.h
@@ -0,0 +1,36 @@
+﻿// Copyright Lim Young.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayInteractionProcessor.h"
+#include "GameplayInteractionProcessor_Press.generated.h"
+
+/**
+ * This class is responsible for processing press input events in the Gameplay Interaction System.
+ */
+UCLASS(DisplayName = "Press")
+class GAMEPLAYINTERACTIONSYSTEM_API UGameplayInteractionProcessor_Press : public UGameplayInteractionProcessor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	FGameplayTag PressInputTag;
+
+	UPROPERTY(EditAnywhere)
+	bool bAccumulatePress = false;
+
+	UPROPERTY(EditAnywhere)
+	float Timeout = 0.0f;
+
+private:
+	FTimerHandle PressTimeoutHandle;
+	void OnPressTimeout() const;
+
+public:
+	virtual void PostInitialize_Implementation() override;
+	virtual void OnGameplayInputEvent_Implementation(const FGameplayTag& InputTag,
+	                                                 const EGameplayInputType InputType) override;
+	virtual void PreCleanup_Implementation() override;
+};
