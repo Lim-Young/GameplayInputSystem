c7770c25aa95792ac19725b91a35c3ed55bfebd9
Lim Young <lim.young@foxmail.com>
2025-06-25 22:45:08 +0800

    Add GameplayInteractableComponent

---
+++ Source/GameplayInteractionSystem/Private/Component/GameplayInteractableComponent.cpp
@@ -0,0 +1,212 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Component/GameplayInteractableComponent.h"
+
+#include "GameplayInteractionSystemLibrary.h"
+#include "Component/GameplayInteractorComponent.h"
+
+
+// Sets default values for this component's properties
+UGameplayInteractableComponent::UGameplayInteractableComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UGameplayInteractableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!InteractionDescription.Valid())
+	{
+		return;
+	}
+
+	if (!InteractionWidgetName.IsNone())
+	{
+		TArray<UWidgetComponent*> WidgetComponents;
+		GetOwner()->GetComponents<UWidgetComponent>(WidgetComponents);
+		auto ResultWidgetComponent = WidgetComponents.FindByPredicate(
+			[&](const UWidgetComponent* WidgetComponent)
+			{
+				return WidgetComponent && WidgetComponent->GetFName() == InteractionWidgetName;
+			});
+
+		if (ResultWidgetComponent)
+		{
+			InteractionWidgetComponent = *ResultWidgetComponent;
+		}
+	}
+	//
+	// if (InteractableCollisionComponentTags.Num() > 0)
+	// {
+	// 	GetOwner()->GetComponents<UPrimitiveComponent>(CollisionComponents);
+	// 	for (UPrimitiveComponent* CollisionComponent : CollisionComponents)
+	// 	{
+	// 		check(CollisionComponent);
+	// 		CollisionComponent->OnComponentBeginOverlap.AddDynamic(
+	// 			this, &UGameplayInteractableComponent::OnComponentBeginOverlap);
+	// 		CollisionComponent->OnComponentEndOverlap.AddDynamic(
+	// 			this, &UGameplayInteractableComponent::OnComponentEndOverlap);
+	// 	}
+	// }
+}
+
+void UGameplayInteractableComponent::BeginDestroy()
+{
+	Super::BeginDestroy();
+
+	// if (CollisionComponents.Num() > 0)
+	// {
+	// 	for (UPrimitiveComponent* CollisionComponent : CollisionComponents)
+	// 	{
+	// 		if (!IsValid(CollisionComponent))
+	// 		{
+	// 			continue;
+	// 		}
+	//
+	// 		CollisionComponent->OnComponentBeginOverlap.RemoveDynamic(
+	// 			this, &UGameplayInteractableComponent::OnComponentBeginOverlap);
+	// 		CollisionComponent->OnComponentEndOverlap.RemoveDynamic(
+	// 			this, &UGameplayInteractableComponent::OnComponentEndOverlap);
+	// 	}
+	// 	CollisionComponents.Empty();
+	// }
+}
+
+// void UGameplayInteractableComponent::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+//                                                              AActor* OtherActor, UPrimitiveComponent* OtherComp,
+//                                                              int32 OtherBodyIndex, bool bFromSweep,
+//                                                              const FHitResult& SweepResult)
+// {
+// 	if (!IsValid(OtherActor))
+// 	{
+// 		return;
+// 	}
+//
+// 	UGameplayInteractorComponent* Interactor = OtherActor->FindComponentByClass<UGameplayInteractorComponent>();
+// 	if (!IsValid(Interactor))
+// 	{
+// 		return;
+// 	}
+//
+// 	if (CheckInteractorTags(Interactor->InteractorTags))
+// 	{
+// 		CurrentInteractor = Interactor;
+// 		ActiveInteraction(Interactor);
+// 	}
+// }
+//
+// void UGameplayInteractableComponent::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+//                                                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+// {
+// 	if (!IsValid(OtherActor))
+// 	{
+// 		return;
+// 	}
+//
+// 	UGameplayInteractorComponent* Interactor = OtherActor->FindComponentByClass<UGameplayInteractorComponent>();
+// 	if (!IsValid(Interactor))
+// 	{
+// 		return;
+// 	}
+//
+// 	DeactivateInteraction();
+// }
+
+bool UGameplayInteractableComponent::CheckInteractorTags(const FGameplayTagContainer& InteractorTags) const
+{
+	if (RequiredInteractorTags.Num() > 0)
+	{
+		if (!RequiredInteractorTags.HasAnyExact(InteractorTags))
+		{
+			return false;
+		}
+	}
+
+	if (IgnoredInteractorTags.Num() > 0)
+	{
+		if (IgnoredInteractorTags.HasAnyExact(InteractorTags))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool UGameplayInteractableComponent::ActiveInteraction(UGameplayInteractorComponent* InteractorComponent)
+{
+	if (!IsValid(InteractorComponent))
+	{
+		return false;
+	}
+
+	CurrentInteractor = InteractorComponent;
+
+	GameplayInteractionInstance = UGameplayInteractionSystemLibrary::CreateGameplayInteractionInstance(
+		this, InteractionDescription, InteractionWidgetComponent);
+
+	if (IsValid(GameplayInteractionInstance))
+	{
+		InteractionState = EGameplayInteractionState::Active;
+
+		GameplayInteractionInstance->OnInteractionInputEvent.AddDynamic(
+			this, &UGameplayInteractableComponent::ProcessInteractionInputEvent);
+		GameplayInteractionInstance->OnInteractionSuccess.AddDynamic(
+			this, &UGameplayInteractableComponent::ProcessInteractionSuccess);
+		GameplayInteractionInstance->OnInteractionFailed.AddDynamic(
+			this, &UGameplayInteractableComponent::ProcessInteractionFailed);
+		GameplayInteractionInstance->OnInteractionComplete.AddDynamic(
+			this, &UGameplayInteractableComponent::ProcessInteractionComplete);
+
+		return true;
+	}
+
+	return false;
+}
+
+bool UGameplayInteractableComponent::DeactivateInteraction()
+{
+	if (InteractionState == EGameplayInteractionState::Inactive)
+	{
+		return true; // Already inactive, nothing to do
+	}
+
+	if (!IsValid(GameplayInteractionInstance))
+	{
+		InteractionState = EGameplayInteractionState::Inactive;
+		return true;
+	}
+
+	GameplayInteractionInstance->DestroyInteractionInstance();
+
+	InteractionState = EGameplayInteractionState::Inactive;
+
+	CurrentInteractor = nullptr;
+	GameplayInteractionInstance = nullptr;
+	return true;
+}
+
+void UGameplayInteractableComponent::ProcessInteractionInputEvent(const FGameplayTag& InputTag,
+                                                                  const EGameplayInputType InputType)
+{
+	OnInteractionInputEvent.Broadcast(InputTag, InputType);
+}
+
+void UGameplayInteractableComponent::ProcessInteractionSuccess()
+{
+	OnInteractionSuccess.Broadcast();
+}
+
+void UGameplayInteractableComponent::ProcessInteractionFailed()
+{
+	OnInteractionFailed.Broadcast();
+}
+
+void UGameplayInteractableComponent::ProcessInteractionComplete()
+{
+	OnInteractionComplete.Broadcast();
+}
---
+++ Source/GameplayInteractionSystem/Public/Component/GameplayInteractableComponent.h
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayInteractionInstance.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "GameplayInteractableComponent.generated.h"
+
+class UGameplayInteractorComponent;
+
+UENUM()
+enum class EGameplayInteractionState : uint8
+{
+	Inactive,
+	Active,
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAMEPLAYINTERACTIONSYSTEM_API UGameplayInteractableComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGameplayInteractableComponent();
+	virtual void BeginPlay() override;
+	virtual void BeginDestroy() override;
+
+// private:
+// 	UFUNCTION()
+// 	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+// 	                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+// 	                             const FHitResult& SweepResult);
+// 	UFUNCTION()
+// 	void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+// 	                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction")
+	FGameplayTagContainer RequiredInteractorTags;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction")
+	FGameplayTagContainer IgnoredInteractorTags;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction")
+	FGameplayInteractionDescription InteractionDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction|Widget")
+	FName InteractionWidgetName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction|Collision")
+	TArray<FName> InteractableCollisionComponentTags;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction|Highlight")
+	TObjectPtr<UMaterialInterface> HighlightMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction|Highlight")
+	TArray<FName> HighlightMeshComponentTags;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction|Events")
+	FOnInteractionComplete OnInteractionSuccess;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction|Events")
+	FOnInteractionFailed OnInteractionFailed;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction|Events")
+	FOnInteractionComplete OnInteractionComplete;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction|Events")
+	FOnInteractionInputEvent OnInteractionInputEvent;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction|Read Only")
+	EGameplayInteractionState InteractionState = EGameplayInteractionState::Inactive;
+
+	UPROPERTY()
+	TObjectPtr<UWidgetComponent> InteractionWidgetComponent;
+
+	TArray<TObjectPtr<UPrimitiveComponent>> CollisionComponents;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UMeshComponent>> HighlightMeshComponents;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UGameplayInteractorComponent> CurrentInteractor;
+
+	UPROPERTY()
+	UGameplayInteractionInstance* GameplayInteractionInstance;
+
+public:
+	bool CheckInteractorTags(const FGameplayTagContainer& InteractorTags) const;
+	bool ActiveInteraction(UGameplayInteractorComponent* InteractorComponent);
+	bool DeactivateInteraction();
+
+private:
+	UFUNCTION()
+	void ProcessInteractionInputEvent(const FGameplayTag& InputTag, const EGameplayInputType InputType);
+	UFUNCTION()
+	void ProcessInteractionSuccess();
+	UFUNCTION()
+	void ProcessInteractionFailed();
+	UFUNCTION()
+	void ProcessInteractionComplete();
+};
