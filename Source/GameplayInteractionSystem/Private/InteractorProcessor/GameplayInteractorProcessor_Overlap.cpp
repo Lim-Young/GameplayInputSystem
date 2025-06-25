7e63c8ff3536478ca1f647a6a8824899c165b326
Lim Young <lim.young@foxmail.com>
2025-06-25 22:48:50 +0800

    Add a GameplayInteractorProcessor of the Overlap type

---
+++ Source/GameplayInteractionSystem/Private/InteractorProcessor/GameplayInteractorProcessor_Overlap.cpp
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "InteractorProcessor/GameplayInteractorProcessor_Overlap.h"
+
+#include "Component/GameplayInteractorComponent.h"
+
+void UGameplayInteractorProcessor_Overlap::PostInitialize_Implementation()
+{
+	if (OverlapComponentTags.Num() <= 0)
+	{
+		return;
+	}
+
+	for (const FName Tag : OverlapComponentTags)
+	{
+		TArray<UActorComponent*> OverlapComponents = OwnerActor->GetComponentsByTag(
+			UPrimitiveComponent::StaticClass(), Tag);
+
+		for (UActorComponent* Component : OverlapComponents)
+		{
+			if (UPrimitiveComponent* PrimitiveComponent = Cast<UPrimitiveComponent>(Component))
+			{
+				if (ActiveOverlapComponents.Contains(PrimitiveComponent))
+				{
+					return;
+				}
+
+				PrimitiveComponent->OnComponentBeginOverlap.AddDynamic(
+					this, &UGameplayInteractorProcessor_Overlap::OnOverlapBegin);
+				PrimitiveComponent->OnComponentEndOverlap.AddDynamic(
+					this, &UGameplayInteractorProcessor_Overlap::OnOverlapEnd);
+
+				ActiveOverlapComponents.AddUnique(PrimitiveComponent);
+			}
+		}
+	}
+}
+
+void UGameplayInteractorProcessor_Overlap::Cleanup_Implementation()
+{
+	if (ActiveOverlapComponents.Num() <= 0)
+	{
+		return;
+	}
+
+	for (UPrimitiveComponent* PrimitiveComponent : ActiveOverlapComponents)
+	{
+		if (IsValid(PrimitiveComponent))
+		{
+			PrimitiveComponent->OnComponentBeginOverlap.RemoveDynamic(
+				this, &UGameplayInteractorProcessor_Overlap::OnOverlapBegin);
+			PrimitiveComponent->OnComponentEndOverlap.RemoveDynamic(
+				this, &UGameplayInteractorProcessor_Overlap::OnOverlapEnd);
+		}
+	}
+
+	ActiveOverlapComponents.Empty();
+}
+
+void UGameplayInteractorProcessor_Overlap::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+                                                          bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor))
+	{
+		return;
+	}
+
+	TArray<UGameplayInteractableComponent*> InteractableComponents;
+	OtherActor->GetComponents<UGameplayInteractableComponent>(InteractableComponents);
+
+	for (const auto InteractableComponent : InteractableComponents)
+	{
+		// TODO: Add Priority Or Predicate Condition
+
+		if (InteractableComponent->CheckInteractorTags(OwnerInteractorComponent->InteractorTags))
+		{
+			ActiveInteractableComponent(InteractableComponent);
+			return;
+		}
+	}
+}
+
+void UGameplayInteractorProcessor_Overlap::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!IsValid(OtherActor))
+	{
+		return;
+	}
+
+	TArray<UGameplayInteractableComponent*> InteractableComponents;
+	OtherActor->GetComponents<UGameplayInteractableComponent>(InteractableComponents);
+
+	for (const auto InteractableComponent : InteractableComponents)
+	{
+		if (InteractableComponent->CheckInteractorTags(OwnerInteractorComponent->InteractorTags))
+		{
+			DeactivateInteractableComponent(InteractableComponent);
+			return;
+		}
+	}
+}
---
+++ Source/GameplayInteractionSystem/Public/InteractorProcessor/GameplayInteractorProcessor_Overlap.h
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayInteractorProcessor.h"
+#include "GameplayInteractorProcessor_Overlap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYINTERACTIONSYSTEM_API UGameplayInteractorProcessor_Overlap : public UGameplayInteractorProcessor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Gameplay Interaction")
+	TArray<FName> OverlapComponentTags;
+
+	virtual void PostInitialize_Implementation() override;
+	virtual void Cleanup_Implementation() override;
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<UPrimitiveComponent>> ActiveOverlapComponents;
+
+protected:
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                  int32 OtherBodyIndex);
+};
