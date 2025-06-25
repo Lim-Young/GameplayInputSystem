b71dde8674e8a3a091037c94a70127287bed465b
Lim Young <lim.young@foxmail.com>
2025-06-25 20:16:43 +0800

    Add GameplayInteractorComponent

---
+++ Source/GameplayInteractionSystem/Private/Component/GameplayInteractorComponent.cpp
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "Component/GameplayInteractorComponent.h"
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayInteraction_Interactor, "GameplayInteraction.Interactor",
+                               "Tag used to identify gameplay interaction interactor components. This tag is used to differentiate between interactor components and other types of components.")
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GameplayInteraction_Interactor_Player, "GameplayInteraction.Interactor.Player",
+                               "Tag used to identify player interactor components. This tag is used to differentiate between player interactor components and other types of interactor components.")
+
+UGameplayInteractorComponent::UGameplayInteractorComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UGameplayInteractorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActiveInteractableComponents.Empty();
+
+	ParallelFor(Processors.Num(), [&](int32 Index)
+	{
+		if (UGameplayInteractorProcessor* Processor = Processors[Index])
+		{
+			Processor->Initialize(this);
+		}
+	});
+}
+
+// Called every frame
+void UGameplayInteractorComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                 FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	ParallelFor(Processors.Num(), [&](int32 Index)
+	{
+		if (UGameplayInteractorProcessor* Processor = Processors[Index])
+		{
+			if (Processor->bTickable)
+			{
+				Processor->Tick(DeltaTime);
+			}
+		}
+	});
+}
+
+void UGameplayInteractorComponent::BeginDestroy()
+{
+	Super::BeginDestroy();
+
+	ParallelFor(Processors.Num(), [&](int32 Index)
+	{
+		if (UGameplayInteractorProcessor* Processor = Processors[Index])
+		{
+			Processor->Cleanup();
+		}
+	});
+}
+
+void UGameplayInteractorComponent::AddInteractableComponent(UGameplayInteractableComponent* InteractableComponent)
+{
+	if (ActiveInteractableComponents.Contains(InteractableComponent))
+	{
+		return;
+	}
+
+	ActiveInteractableComponents.Add(InteractableComponent);
+
+	OnInteractableComponentAdded.Broadcast(InteractableComponent);
+}
+
+void UGameplayInteractorComponent::RemoveInteractableComponent(UGameplayInteractableComponent* InteractableComponent)
+{
+	if (!ActiveInteractableComponents.Contains(InteractableComponent))
+	{
+		return;
+	}
+
+	PreInteractableComponentRemove.Broadcast(InteractableComponent);
+
+	ActiveInteractableComponents.Remove(InteractableComponent);
+}
---
+++ Source/GameplayInteractionSystem/Public/Component/GameplayInteractorComponent.h
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayInteractableComponent.h"
+#include "GameplayInteractorProcessor.h"
+#include "NativeGameplayTags.h"
+#include "Components/ActorComponent.h"
+#include "GameplayInteractorComponent.generated.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayInteraction_Interactor);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(GameplayInteraction_Interactor_Player);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractableComponentAdded, UGameplayInteractableComponent*,
+                                            InteractableComponent);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPreInteractableComponentRemove, UGameplayInteractableComponent*,
+                                            InteractableComponent);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAMEPLAYINTERACTIONSYSTEM_API UGameplayInteractorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGameplayInteractorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void BeginDestroy() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay Interaction", Instanced)
+	TArray<TObjectPtr<UGameplayInteractorProcessor>> Processors;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTagContainer InteractorTags;
+
+private:
+	TArray<TObjectPtr<UGameplayInteractableComponent>> ActiveInteractableComponents;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction")
+	FOnInteractableComponentAdded OnInteractableComponentAdded;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay Interaction")
+	FPreInteractableComponentRemove PreInteractableComponentRemove;
+
+	void AddInteractableComponent(UGameplayInteractableComponent* InteractableComponent);
+	void RemoveInteractableComponent(UGameplayInteractableComponent* InteractableComponent);
+};
