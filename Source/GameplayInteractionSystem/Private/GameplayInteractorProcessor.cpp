31170fb2c006939faaa12a347f7b03dc1c5844e0
Lim Young <lim.young@foxmail.com>
2025-06-25 22:47:08 +0800

    Add GameplayInteractorProcessor

---
+++ Source/GameplayInteractionSystem/Private/GameplayInteractorProcessor.cpp
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Lim Young
+
+
+#include "GameplayInteractorProcessor.h"
+
+#include "Component/GameplayInteractorComponent.h"
+
+void UGameplayInteractorProcessor::Initialize(UGameplayInteractorComponent* GameplayInteractorComponent)
+{
+	OwnerInteractorComponent = GameplayInteractorComponent;
+	check(OwnerInteractorComponent);
+
+	OwnerActor = OwnerInteractorComponent->GetOwner();
+
+	PostInitialize();
+}
+
+void UGameplayInteractorProcessor::PostInitialize_Implementation()
+{
+}
+
+void UGameplayInteractorProcessor::Tick_Implementation(float DeltaTime)
+{
+}
+
+void UGameplayInteractorProcessor::Cleanup_Implementation()
+{
+}
+
+void UGameplayInteractorProcessor::ActiveInteractableComponent(
+	UGameplayInteractableComponent* InteractableComponent) const
+{
+	if (!IsValid(InteractableComponent))
+	{
+		return;
+	}
+
+	InteractableComponent->ActiveInteraction(OwnerInteractorComponent);
+	OwnerInteractorComponent->AddInteractableComponent(InteractableComponent);
+}
+
+void UGameplayInteractorProcessor::DeactivateInteractableComponent(
+	UGameplayInteractableComponent* InteractableComponent) const
+{
+	if (!IsValid(InteractableComponent))
+	{
+		return;
+	}
+
+	InteractableComponent->DeactivateInteraction();
+	OwnerInteractorComponent->RemoveInteractableComponent(InteractableComponent);
+}
---
+++ Source/GameplayInteractionSystem/Public/GameplayInteractorProcessor.h
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameplayInteractorProcessor.generated.h"
+
+class UGameplayInteractorComponent;
+
+/**
+ * 
+ */
+UCLASS(Abstract, DefaultToInstanced, CollapseCategories, EditInlineNew)
+class GAMEPLAYINTERACTIONSYSTEM_API UGameplayInteractorProcessor : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Gameplay Interaction")
+	bool bTickable = false;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<AActor> OwnerActor;
+
+	UPROPERTY()
+	TObjectPtr<UGameplayInteractorComponent> OwnerInteractorComponent;
+
+public:
+	void Initialize(UGameplayInteractorComponent* GameplayInteractorComponent);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Gameplay Interaction")
+	void PostInitialize();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Gameplay Interaction")
+	void Tick(float DeltaTime);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Gameplay Interaction")
+	void Cleanup();
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Interaction")
+	void ActiveInteractableComponent(UGameplayInteractableComponent* InteractableComponent) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Interaction")
+	void DeactivateInteractableComponent(UGameplayInteractableComponent* InteractableComponent) const;
+};
